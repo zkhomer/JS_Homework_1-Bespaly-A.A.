eb3c82cdde5ee8e2ff8acf0bc1de287f2c310b11
Arthur <zkhomers@gmail.com>
2019-12-30 22:52:00 +0200

    bug-fix

--- 04/main.js
+++ 04/main.js
@@ -1,4 +1,12 @@
+/*
 var aa = 13
 var bb = 12
 var cc = 11
-
+console.log (Math.max(aa, bb, cc)+" "+"макс значение")
+console.log( Math.min(aa, bb, cc)+" "+"мин значение" )     так былобы круче  ))   */
+var aa = 13
+var bb = 12
+var cc = 11
+if(cc<bb , cc<aa){
+    console.log("cc = 11")
+}
