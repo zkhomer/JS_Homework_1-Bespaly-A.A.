c6646801117b7ae7debcd3130abded01d5936451
Arthur <zkhomers@gmail.com>
2019-12-30 17:40:24 +0200

    save

---
+++ 03/main.js
@@ -0,0 +1,6 @@
+var numbers = [ 254, 115, 78, 25, 91, 45, 37 ]
+var out = []
+for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i]>=50 ) out.push(numbers[i])
+}
+console.log(out)
---
+++ 05/main.js
@@ -0,0 +1,9 @@
+var age = 24
+var day ="воскресенье"
+
+if((age>=20) && (age<=27))
+{
+    console.warn("Выслать повестку")}
+else{
+    console.warn("не высылать повестку")
+}
