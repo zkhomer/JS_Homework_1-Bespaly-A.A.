687c987f2c10d4832b6dd6a5d5b50e52c8041ebb
Arthur <zkhomers@gmail.com>
2020-01-02 15:14:50 +0200

    задание 3. создал 2 масива 1 пустой циклом фор перебрал массив в котором были несортированные числа и с помощью out.push запушил все числа которые были больше или равны 50 в пустой массив Out после чего вывел его на экран

--- 03/main.js
+++ 03/main.js
@@ -1,6 +1,8 @@
-var numbers = [ 254, 115, 78, 25, 91, 45, 37 ]
+var numbers = [ 254, 115, 78, 25, 91, 45, 37]
 var out = []
 for (var i = 0; i < numbers.length; i++) {
     if (numbers[i]>=50 ) out.push(numbers[i])
 }
 console.log(out)
+/*
+asda*/
