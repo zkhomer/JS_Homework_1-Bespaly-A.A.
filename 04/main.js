5f3fa92a9f0627d8f423ece36d8772deb9135fe1
Arthur <zkhomers@gmail.com>
2020-01-02 15:18:25 +0200

    задание 4. создал 3 переменных потом сравнил одну из них с 2мя другими и нашел наименьшую после чего вывел ее использовал оператор if

--- 04/main.js
+++ 04/main.js
@@ -3,7 +3,7 @@ var aa = 13
 var bb = 12
 var cc = 11
 console.log (Math.max(aa, bb, cc)+" "+"макс значение")
-console.log( Math.min(aa, bb, cc)+" "+"мин значение" )     так былобы круче  ))   */
+console.log( Math.min(aa, bb, cc)+" "+"мин значение" )     так былобы круче  ))))   */
 var aa = 13
 var bb = 12
 var cc = 11
