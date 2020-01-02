fdb9e9b3bbc3e053e4da816dd23e87042db8e024
Arthur <zkhomers@gmail.com>
2020-01-02 15:33:04 +0200

    задание 8. тут я создал калкуляцию которая считает количество отжиманий использовал значение переменной и умножение или деление

--- 08/main.js
+++ 08/main.js
@@ -6,5 +6,5 @@ var oneDay = 100 // количество отжиманий в день
 var oneWeek = oneDay*7 // количество отжиманий в неделю
 var oneMonth = oneWeek*4 // количество отжиманий в месс
 var oneYear = oneMonth*12 // количество отжиманий в год
-var sixMonth = oneYear/2 // количество отжиманий за пол года
+var sixMonth = oneYear/2 // количество отжиманий за пол года)
 console.log(tryinner +" "+sixMonth+" "+tryinners )
